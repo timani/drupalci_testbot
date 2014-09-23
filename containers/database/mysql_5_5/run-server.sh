442ab98d95d723fd97e460f7a806885513b11556
Timani Tunduwani <timani@users.noreply.github.com>
2014-09-23 09:42:13 -0700

    Remove whoami from database mysql_5_5 #default run-server.sh

--- containers/database/mysql_5_5/run-server.sh
+++ containers/database/mysql_5_5/run-server.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Check if we are a member of the "docker" group or have root powers
-USERNAME=$(whoami)
-TEST=$(groups $USERNAME | grep -c '\bdocker\b')
-if [ $TEST -eq 0 ];
-then
-  if [ `whoami` != root ]; then
-    echo "Please run this script as root or using sudo"
-    exit 1
-  fi
-fi
-
-
 TAG="drupal/testbot-mysql_5_5"
 NAME="drupaltestbot-db-mysql_5_5"
 STALLED=$(docker ps -a | grep ${TAG} | grep Exit | awk '{print $1}')
