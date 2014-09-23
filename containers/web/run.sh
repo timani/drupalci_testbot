e0d24a8b90e46f8266fd450dd1e242ee36a8ab79
Timani Tunduwani <timani@users.noreply.github.com>
2014-09-23 09:55:27 -0700

    Remove whoami from web run.sh

--- containers/web/run.sh
+++ containers/web/run.sh
@@ -149,12 +149,6 @@ esac
 
 RUNSCRIPT=${RUNSCRIPT:-"php ${RUNNER} --php /usr/bin/php --url 'http://localhost' --color --concurrency ${CONCURRENCY} ${VERBO} --xml '/var/workspace/results'"}
 
-# Check if we have root powers
-if [ `whoami` != root ]; then
-    echo "Please run this script as root or using sudo"
-    exit 1
-fi
-
 mkdir -p ${REPODIR}
 
 # Check if we have free disk space
