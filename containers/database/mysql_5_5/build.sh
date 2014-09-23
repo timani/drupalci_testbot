39b5a69e8f37e10e049f020189227adc1c85bb04
Timani Tunduwani <timani@users.noreply.github.com>
2014-09-23 09:53:05 -0700

    Remove whoami from database mysql_5_5 build.sh

--- containers/database/mysql_5_5/build.sh
+++ containers/database/mysql_5_5/build.sh
@@ -3,17 +3,6 @@
 # Remove intermediate containers after a successful build. Default is True.
 DCI_REMOVEINTCONTAINERS=${DCI_REMOVEINTCONTAINERS:-"true"}
 
-# Check if we are a member of the "docker" group or have root powers
-USERNAME=$(whoami)
-TEST=$(groups $USERNAME | grep -c '\bdocker\b')
-if [ $TEST -eq 0 ];
-then
-  if [ `whoami` != root ]; then
-    echo "Please run this script as root or using sudo"
-    exit 1
-  fi
-fi
-
 docker ps | grep "drupal/testbot-mysql_5_5" | awk '{print $1}' | grep -v CONTAINER | xargs -n1 -I {} sudo docker stop {}
 docker ps -a | grep "drupal/testbot-mysql_5_5" | awk '{print $1}' | grep -v CONTAINER | xargs -n1 -I {} sudo docker rm {}
 
